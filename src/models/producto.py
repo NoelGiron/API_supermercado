d35bb2611126b2fc74e4697787e4a84637e19ae8
NoelGiron <mngiron@hotmail.com>
2025-10-12 22:23:55 -0600

    estructura base de para flask

---
+++ src/models/producto.py
@@ -0,0 +1,9 @@
+class producto:
+    def __init__(self, nombre, categoria, descripcion, precio, cantidad, vencimineto):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.descripcion = descripcion
+        self.precio = precio
+        self.categoria = categoria
+        self.cantidad = cantidad
+        self.vencimiento = vencimineto
